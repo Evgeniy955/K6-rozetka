df6154a3f4c94866c03d9de5448b596ce8034da2
Yevhen335 <y.halitsyn@elsevier.com>
2025-06-18 13:43:10 +0300

    update smoke test to increase VUs and change target URL

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -4,12 +4,12 @@ import { handleSummary } from '../summary.js';
 
 // Smoke-тест: минимальная нагрузка для проверки работоспособности страницы
 export const options = {
-    vus: 1,
+    vus: 10,
     duration: '10s',
 };
 
 export default function () {
-    const res = http.get('https://rozetka.com.ua/ua/hator-hta-914/p453054884/');
+    const res = http.get('https://elmir.ua/cell_phones/mobile-phone-samsung-galaxy-s25-ultra-12gb-1tb-titanium-black-sm-s938bzkheuc.html');
     check(res, { 'статус 200': (r) => r.status === 200 });
     sleep(1); // пауза между запросами
 }
